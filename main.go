78dd963f7ee36cc3b78d6115c1f6717b17f4d666
AndreaGhizzoni <andrea.ghz@gmail.com>
2017-01-23 17:40:56 +0100

    refactoring and added some comments

--- main.go
+++ main.go
@@ -1,17 +1,13 @@
 package main
 
-import "fmt"
-
-const (
-	P = "lasciaperderequesto"
-	T = "questoeunesempio"
-
-	/*
-		P = "aaabbbccc"
-		T = "zzzbbbttt"
-	*/
+import (
+	"flag"
+	"fmt"
 )
 
+var P, T = "", ""
+
+// TODO using built in max function
 func max(x, y int) int {
 	if x >= y {
 		return x
@@ -20,6 +16,7 @@ func max(x, y int) int {
 	}
 }
 
+// Functon that calculates the Longest common Subsequence between P and T.
 func lcs(d *[][]int, i, j int) int {
 	if i < 0 || j < 0 {
 		return 0
@@ -37,21 +34,30 @@ func lcs(d *[][]int, i, j int) int {
 	}
 }
 
+// Function that prints the longest common subsequences between P and T.
 func printdiff(d *[][]int, i, j int) {
 	D := *d
 	if i >= 0 && j >= 0 && P[i] == T[j] {
 		printdiff(d, i-1, j-1)
 		fmt.Printf("%c", P[i])
-	} else if j >= 0 && (i < 0 || D[i][j-1] >= D[i-1][j]) {
+	} else if j > 0 && (i == 0 || D[i][j-1] >= D[i-1][j]) {
 		printdiff(d, i, j-1)
 		//fmt.Printf("%c", T[i])
-	} else if i >= 0 && (j < 0 || D[i][j-1] < D[i-1][j]) {
+	} else if i > 0 && (j == 0 || D[i][j-1] < D[i-1][j]) {
 		printdiff(d, i-1, j)
 		//fmt.Printf("%c", P[i])
 	}
 }
 
 func main() {
+	PFlag := flag.String("s1", "", "first string")
+	TFlag := flag.String("s2", "", "second string")
+
+	flag.Parse()
+
+	P = *PFlag
+	T = *TFlag
+
 	lenP := len(P)
 	lenT := len(T)
 	D := make([][]int, lenP)
